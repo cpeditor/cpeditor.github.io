30f23ca06307aae6281794ab7f98263031f1e246
Ashar Khan <ashar786khan@gmail.com>
2020-02-25 03:03:04 +0530

    Add cp-editor website
    This is official website source code, to be hosted on github

---
+++ js/api/downloads.js
@@ -0,0 +1,71 @@
+
+function getOS() {
+    var userAgent = window.navigator.userAgent,
+        platform = window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        os = null;
+
+    if (macosPlatforms.indexOf(platform) !== -1) {
+        os = 'Mac OS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+        os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+        os = 'Android';
+    } else if (!os && /Linux/.test(platform)) {
+        os = 'Linux';
+    }
+
+    return os;
+}
+
+function fetch_latest_release() {
+
+    let defaultUrl = 'https://github.com/cpeditor/cp-editor/releases/latest'
+
+    document.getElementById('download_windows').setAttribute('href', defaultUrl)
+    document.getElementById('download_mac').setAttribute('href', defaultUrl)
+    document.getElementById('download_linux').setAttribute('href', defaultUrl)
+
+    let uri = 'https://api.github.com/repos/cpeditor/cp-editor/releases/latest'
+
+    fetch(uri).then((response) => {
+        return response.json()
+    }).then((data) => {
+
+        os = getOS()
+        let directUrl = defaultUrl
+
+        data.assets.forEach(release => {
+            if (release.name.endsWith(".exe")) {
+                document.getElementById('download_windows').setAttribute('href', release.browser_download_url)
+                if (os === "Windows") directUrl = release.browser_download_url
+            }
+            if (release.name.endsWith(".dmg")) {
+                document.getElementById('download_mac').setAttribute('href', release.browser_download_url)
+                if (os === "Mac OS") directUrl = release.browser_download_url
+            }
+            if (release.name.endsWith(".AppImage")) {
+                document.getElementById('download_linux').setAttribute('href', release.browser_download_url)
+                if (os === "Windows") directUrl = release.browser_download_url
+            }
+        });
+
+        if (os === "Windows" || os === "Linux" || os === "Mac OS") {
+            document.getElementById('download_by_os').setAttribute('href', directUrl)
+            document.getElementById('download_by_os').innerText = 'Get for ' + getOS()
+
+            document.getElementById('download_by_os_bottom').setAttribute('href', directUrl)
+            document.getElementById('download_by_os_bottom').innerText = 'Try it on ' + getOS()
+            
+        }
+
+    })
+}
+
+fetch_latest_release()
+
+
