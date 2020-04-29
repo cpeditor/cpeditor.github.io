5a20195187273db762e650bdd4691b3afdf8dfeb
ouuan <ouuan@users.noreply.github.com>
2020-04-29 01:12:16 +0000

    Add auto deploy 9f865c7cf0a5c4f0ff52cc2324af99fc9de8d5a2

--- js/theme.js
+++ js/theme.js
@@ -1,26 +1,6 @@
 ;(function($){
     "use strict"
 	
-	
-	var nav_offset_top = $('header').height() + 50; 
-    /*-------------------------------------------------------------------------------
-	  Navbar 
-	-------------------------------------------------------------------------------*/
-
-	//* Navbar Fixed  
-    function navbarFixed(){
-        if ( $('.header_area').length ){ 
-            $(window).scroll(function() {
-                var scroll = $(window).scrollTop();   
-                if (scroll >= nav_offset_top ) {
-                    $(".header_area").addClass("navbar_fixed");
-                } else {
-                    $(".header_area").removeClass("navbar_fixed");
-                }
-            });
-        };
-    };
-    navbarFixed();
 	/*----------------------------------------------------*/
     /*  Testimonials Slider
     /*----------------------------------------------------*/
