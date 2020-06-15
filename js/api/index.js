aaf029f39add6c74e62a5f087d5ba72743f75a27
ouuan <ouuan@users.noreply.github.com>
2020-06-15 23:37:17 +0000

    Fix warnings of the JS codes (#36)
    * Fix warnings of the JS codes
    
    * Fix warnings of the JS codes
    
    * Fix warnings of the JS codes
    
    * Fix warnings of the JS codes 2e7340a757d1156140a0372c787756ab377b97c0

--- js/api/index.js
+++ js/api/index.js
@@ -1,53 +1,59 @@
 function getOS() {
     var userAgent = window.navigator.userAgent,
         platform = window.navigator.platform,
-        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
+        windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
+        iosPlatforms = ["iPhone", "iPad", "iPod"],
         os = null;
 
     if (macosPlatforms.indexOf(platform) !== -1) {
-        os = 'Mac OS';
+        os = "Mac OS";
     } else if (iosPlatforms.indexOf(platform) !== -1) {
-        os = 'iOS';
+        os = "iOS";
     } else if (windowsPlatforms.indexOf(platform) !== -1) {
-        os = 'Windows';
+        os = "Windows";
     } else if (/Android/.test(userAgent)) {
-        os = 'Android';
-    } else if (!os && /Linux/.test(platform)) {
-        os = 'Linux';
+        os = "Android";
+    } else if (/Linux/.test(platform)) {
+        os = "Linux";
     }
 
     return os;
 }
 
-function fetch_latest_release() {
-    let uri = 'https://api.github.com/repos/cpeditor/cpeditor/releases/latest'
+function fetchLatestRelease() {
+    let uri = "https://api.github.com/repos/cpeditor/cpeditor/releases/latest";
 
     fetch(uri)
-        .then(res => res.json())
+        .then((res) => res.json())
         .then((data) => {
-            os = getOS()
-            let directUrl = uri
+            var os = getOS();
+            let directUrl = uri;
 
-            data.assets.forEach(release => {
+            data.assets.forEach((release) => {
                 if (release.name.endsWith(".exe")) {
-                    if (os === "Windows") directUrl = release.browser_download_url
-                }else if (release.name.endsWith(".dmg")) {
-                    if (os === "Mac OS") directUrl = release.browser_download_url
-                }else if (release.name.endsWith(".AppImage")) {
-                    if (os === "Linux") directUrl = release.browser_download_url
+                    if (os === "Windows") {
+                        directUrl = release.browser_download_url;
+                    }
+                } else if (release.name.endsWith(".dmg")) {
+                    if (os === "Mac OS") {
+                        directUrl = release.browser_download_url;
+                    }
+                } else if (release.name.endsWith(".AppImage")) {
+                    if (os === "Linux") {
+                        directUrl = release.browser_download_url;
+                    }
                 }
             });
 
             if (os === "Windows" || os === "Linux" || os === "Mac OS") {
-                document.getElementById('download_by_os').setAttribute('href', directUrl)
-                document.getElementById('download_by_os').innerText = 'Get for ' + getOS()
+                document.getElementById("download_by_os").setAttribute("href", directUrl);
+                document.getElementById("download_by_os").innerText = "Get for " + getOS();
 
-                document.getElementById('download_by_os_bottom').setAttribute('href', directUrl)
-                document.getElementById('download_by_os_bottom').innerText = 'Try it on ' + getOS()
+                document.getElementById("download_by_os_bottom").setAttribute("href", directUrl);
+                document.getElementById("download_by_os_bottom").innerText = "Try it on " + getOS();
             }
-        })
+        });
 }
 
-fetch_latest_release()
+fetchLatestRelease();
