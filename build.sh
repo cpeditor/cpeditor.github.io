5a0072f197bb480646c563c9fb178425bda6acc6
Yufan You <ouuansteve@gmail.com>
2020-09-26 22:03:52 +0800

    build: use scripts to auto build multiple versions

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+git config core.quotePath false
+
+version_branches="$(git for-each-ref --format='%(refname:short)' refs/heads/v*.*)"
+
+function build() {
+    sed -i "s/baseURL = \"\/\"/baseURL = \"\/$1\"/" config.toml
+
+    echo "[[params.versions]]
+    version = \"master\"
+    url = \"/\"" >> config.toml
+
+    for version in $version_branches; do
+        echo "[[params.versions]]
+        version = \"$version\"
+        url = \"/$version\"" >> config.toml
+    done
+
+    hugo --minify
+
+    git reset --hard
+}
+
+git switch hugo
+rm -rf dist; mkdir -p dist
+build
+mv public/* dist
+
+for branch in $version_branches; do
+    git switch "$branch"
+    git submodule update
+    build $branch
+    mv public "dist/$branch"
+done
