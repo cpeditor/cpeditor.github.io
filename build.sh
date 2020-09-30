7dc8cb8c493ebea1088afe69327084db48d7cc93
Yufan You <ouuansteve@gmail.com>
2020-09-30 22:02:31 +0800

    build: use scripts to auto build multiple versions (#63)
    * build: use scripts to auto build multiple versions
    
    * refactor: simplify the script
    
    * style: fix Codacy warning
    
    * fix: update current version during build
    
    * fix: use the absolute path for latest version
    
    * ci: deploy on version branches
    
    * fix: don't replace url_latest_version
    
    * refactor: use checkout instead of switch
    
    The git on Render doesn't support switch

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+git config core.quotePath false
+
+version_branches="$(git for-each-ref --format='%(refname:short)' refs/heads/v*.*)"
+
+function build() {
+    sed -i "s/baseURL = \"\/\"/baseURL = \"\/$1\"/" config.toml
+
+    if [[ "$1" != "" ]]; then
+        sed -i "s/^version = \".*\"/version = \"$1\"/" config.toml
+    fi
+
+    echo "[[params.versions]]
+    version = \"alpha\"
+    url = \"/\"" >> config.toml
+
+    for version in $version_branches; do
+        echo "[[params.versions]]
+        version = \"$version\"
+        url = \"/$version\"" >> config.toml
+    done
+
+    hugo --minify
+
+    git reset --hard
+}
+
+git checkout hugo
+build
+rm -rf dist; mv public dist
+
+for branch in $version_branches; do
+    git checkout "$branch"
+    git submodule update
+    build "$branch"
+    mv public "dist/$branch"
+done
