ab3f8c0a6b75765a9ff2647154d6c175619b4289
Yufan You <ouuansteve@gmail.com>
2021-02-06 13:59:19 +0800

    feat(download): update download to adapt the new release assets (#141)
    * chore(index): remove links to the download URL & add links to the download page
    
    1. To avoid duplicate codes in both pages.
    2. To let the user choose the asset they want to download.
    
    * style(js): format codes
    
    * refactor(download.js): improve getOS()
    
    * feat(download): choose x64-setup.exe as the best asset for Windows
    
    It seems that we can't reliably distinguish 64-bit Windows and 32-bit
    Windows by JavaScript. As most users use 64-bit Windows, we use it as
    the best asset for all Windows users.
    
    * feat(download): separate versions and assets
    
    * feat(download): adjust margins

--- static/js/download.js
+++ static/js/download.js
@@ -1,142 +1,164 @@
 "use strict";
 
 function versionCompare(v1, v2) {
-    let v1parts = v1.split(".");
-    let v2parts = v2.split(".");
+  let v1parts = v1.split(".");
+  let v2parts = v2.split(".");
 
-    function isValidPart(x) {
-        return (/^\d+$/).test(x);
+  function isValidPart(x) {
+    return /^\d+$/.test(x);
+  }
+
+  if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+    return NaN;
+  }
+
+  while (v1parts.length < v2parts.length) {
+    v1parts.push("0");
+  }
+
+  while (v2parts.length < v1parts.length) {
+    v2parts.push("0");
+  }
+
+  v1parts = v1parts.map(Number);
+  v2parts = v2parts.map(Number);
+
+  for (let i = 0; i < v1parts.length; ++i) {
+    if (v1parts[i] === v2parts[i]) {
+      continue;
+    } else if (v1parts[i] > v2parts[i]) {
+      return 1;
+    } else {
+      return -1;
     }
+  }
 
-    if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
-        return NaN;
-    }
-
-    while (v1parts.length < v2parts.length) {
-        v1parts.push("0");
-    }
-
-    while (v2parts.length < v1parts.length) {
-        v2parts.push("0");
-    }
-
-    v1parts = v1parts.map(Number);
-    v2parts = v2parts.map(Number);
-
-    for (let i = 0; i < v1parts.length; ++i) {
-        if (v1parts[i] === v2parts[i]) {
-            continue;
-        }
-        else if (v1parts[i] > v2parts[i]) {
-            return 1;
-        }
-        else {
-            return -1;
-        }
-    }
-
-    return 0;
+  return 0;
 }
 
 function getOS() {
-    const userAgent = window.navigator.userAgent,
-        platform = window.navigator.platform,
-        macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
-        windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
-        iosPlatforms = ["iPhone", "iPad", "iPod"];
-    let os = null;
+  const userAgent = window.navigator.userAgent;
+  const platform = window.navigator.platform;
+  const macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"];
+  const windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"];
+  const iosPlatforms = ["iPhone", "iPad", "iPod"];
 
-    if (macosPlatforms.indexOf(platform) !== -1) {
-        os = "Mac OS";
-    } else if (iosPlatforms.indexOf(platform) !== -1) {
-        os = "iOS";
-    } else if (windowsPlatforms.indexOf(platform) !== -1) {
-        os = "Windows";
-    } else if (/Android/.test(userAgent)) {
-        os = "Android";
-    } else if (/Linux/.test(platform)) {
-        os = "Linux";
-    }
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    return "macOS";
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    return "iOS";
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    return "Windows";
+  } else if (/Android/.test(userAgent)) {
+    return "Android";
+  } else if (/Linux/.test(platform)) {
+    return "Linux";
+  }
 
-    return os;
+  return null;
 }
 
 $(document).ready(() => {
-    const vm = new Vue({
-        el: "#app",
-        data: {
-            userPlatform: "Unknown",
-            selectedPlatform: "",
-            releases: [],
-            selectedAsset: "",
-            platformOptions: ["Windows", "Linux", "Mac OS"]
-        },
-        methods: {
-            isAssetSuitableForPlatform(asset, platform = this.userPlatform) {
-                const name = asset.name;
-                switch (platform) {
-                    case "Windows":
-                        return name.includes("windows") || name.endsWith("exe");
-                    case "Linux":
-                        return name.includes("linux") || name.endsWith("AppImage");
-                    case "Mac OS":
-                        return name.includes("macos") || name.endsWith("dmg");
-                    default:
-                        return false;
-                }
-            },
-            bestAssetForPlatform(assets, platform) {
-                const suitableAssets = assets.filter((asset) => this.isAssetSuitableForPlatform(asset, platform));
-                if (suitableAssets.length === 0) {
-                    return null;
-                } else if (suitableAssets.length === 1) {
-                    return suitableAssets[0];
-                } else {
-                    switch (platform) {
-                        case "Windows":
-                            return suitableAssets.find((asset) => asset.name.endsWith("exe"));
-                        case "Linux":
-                            return suitableAssets.find((asset) => asset.name.endsWith("AppImage"));
-                        default:
-                            return suitableAssets.find((asset) => !asset.name.includes("portable"));
-                    }
-                }
-            }
-        },
-        computed: {
-            latestStableRelease() {
-                return this.releases.find(({ prerelease }) => !prerelease);
-            },
-            latestStableAssetForUserPlatform() {
-                if (this.latestStableRelease) {
-                    return this.bestAssetForPlatform(this.latestStableRelease.assets, this.userPlatform);
-                } else {
-                    return null;
-                }
-            }
-        },
-        watch: {
-            selectedPlatform(platform) {
-                if (this.latestStableRelease) {
-                    this.selectedAsset = this.bestAssetForPlatform(this.latestStableRelease.assets, platform);
-                }
-            }
-        },
-        created() {
-            this.userPlatform = getOS();
-            if (this.platformOptions.includes(this.userPlatform)) {
-                this.selectedPlatform = this.userPlatform;
-            } else {
-                this.selectedPlatform = this.platformOptions[0];
-            }
-            $.get("https://api.github.com/repos/cpeditor/cpeditor/releases", { per_page: 10 })
-                .then((data) => {
-                    this.releases = data;
-                    if (this.releases.length > 0) {
-                        this.releases.sort((x, y) => versionCompare(x.tag_name, y.tag_name)).reverse();
-                        this.selectedAsset = this.bestAssetForPlatform(this.latestStableRelease.assets, this.selectedPlatform);
-                    }
-                });
+  const vm = new Vue({
+    el: "#app",
+    data: {
+      userPlatform: "Unknown",
+      selectedPlatform: "",
+      releases: [],
+      selectedAsset: null,
+      selectedRelease: null,
+      platformOptions: ["Windows", "Linux", "macOS"],
+    },
+    methods: {
+      isAssetSuitableForPlatform(asset, platform = this.userPlatform) {
+        const name = asset.name;
+        switch (platform) {
+          case "Windows":
+            return name.includes("windows") || name.endsWith("exe");
+          case "Linux":
+            return name.includes("linux") || name.endsWith("AppImage");
+          case "macOS":
+            return name.includes("macos") || name.endsWith("dmg");
+          default:
+            return false;
         }
-    });
+      },
+      bestAssetForPlatform(assets, platform) {
+        const suitableAssets = assets.filter((asset) =>
+          this.isAssetSuitableForPlatform(asset, platform)
+        );
+        if (suitableAssets.length === 0) {
+          return null;
+        } else if (suitableAssets.length === 1) {
+          return suitableAssets[0];
+        } else {
+          switch (platform) {
+            case "Windows":
+              return suitableAssets.find((asset) =>
+                asset.name.endsWith("x64-setup.exe")
+              );
+            case "Linux":
+              return suitableAssets.find((asset) =>
+                asset.name.endsWith("AppImage")
+              );
+            default:
+              return suitableAssets.find(
+                (asset) => !asset.name.includes("portable")
+              );
+          }
+        }
+      },
+      chooseBestAsset() {
+        if (this.selectedRelease) {
+          this.selectedAsset = this.bestAssetForPlatform(
+            this.selectedRelease.assets,
+            this.selectedPlatform
+          );
+        }
+      },
+    },
+    computed: {
+      latestStableRelease() {
+        return this.releases.find(({ prerelease }) => !prerelease);
+      },
+      latestStableAssetForUserPlatform() {
+        if (this.latestStableRelease) {
+          return this.bestAssetForPlatform(
+            this.latestStableRelease.assets,
+            this.userPlatform
+          );
+        } else {
+          return null;
+        }
+      },
+    },
+    watch: {
+      selectedPlatform() {
+        this.chooseBestAsset();
+      },
+      selectedRelease() {
+        this.chooseBestAsset();
+      },
+    },
+    created() {
+      this.userPlatform = getOS();
+      if (this.platformOptions.includes(this.userPlatform)) {
+        this.selectedPlatform = this.userPlatform;
+      } else {
+        this.selectedPlatform = this.platformOptions[0];
+      }
+      $.get("https://api.github.com/repos/cpeditor/cpeditor/releases", {
+        per_page: 10,
+      }).then((data) => {
+        this.releases = data;
+        if (this.releases.length > 0) {
+          this.releases
+            .sort((x, y) => versionCompare(x.tag_name, y.tag_name))
+            .reverse();
+          this.selectedRelease = this.latestStableRelease;
+          this.chooseBestAsset();
+        }
+      });
+    },
+  });
 });
--- static/js/index.js
+++ static/js/index.js
@@ -1,81 +1,22 @@
 "use strict";
 
-function getOS() {
-    var userAgent = window.navigator.userAgent,
-        platform = window.navigator.platform,
-        macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
-        windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
-        iosPlatforms = ["iPhone", "iPad", "iPod"],
-        os = null;
-
-    if (macosPlatforms.indexOf(platform) !== -1) {
-        os = "Mac OS";
-    } else if (iosPlatforms.indexOf(platform) !== -1) {
-        os = "iOS";
-    } else if (windowsPlatforms.indexOf(platform) !== -1) {
-        os = "Windows";
-    } else if (/Android/.test(userAgent)) {
-        os = "Android";
-    } else if (/Linux/.test(platform)) {
-        os = "Linux";
-    }
-
-    return os;
-}
-
-const vm = new Vue({
-    el: "#app",
-    data: {
-        os: getOS(),
-        downloadURL: "https://github.com/cpeditor/cpeditor/releases/latest",
-        assetAvailable: false
+$(document).ready(
+  $(".testi_slider").owlCarousel({
+    loop: true,
+    margin: 30,
+    items: 2,
+    nav: false,
+    autoplay: true,
+    smartSpeed: 1500,
+    dots: false,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
     },
-    created() {
-        Vue.nextTick(() => {
-            if ($(".testi_slider").length) {
-                $(".testi_slider").owlCarousel({
-                    loop: true,
-                    margin: 30,
-                    items: 2,
-                    nav: false,
-                    autoplay: true,
-                    smartSpeed: 1500,
-                    dots: false,
-                    responsiveClass: true,
-                    responsive: {
-                        0: {
-                            items: 1,
-                        },
-                        768: {
-                            items: 2,
-                        },
-                    }
-                });
-            }
-        });
-
-        fetch("https://api.github.com/repos/cpeditor/cpeditor/releases/latest")
-            .then((res) => res.json())
-            .then((data) => {
-                let suffix;
-                switch (this.os) {
-                    case "Windows":
-                        suffix = ".exe";
-                        break;
-                    case "Mac OS":
-                        suffix = ".dmg";
-                        break;
-                    case "Linux":
-                        suffix = ".AppImage";
-                        break;
-                }
-
-                const suitableAsset = data.assets.find((asset) => asset.name.endsWith(suffix));
-
-                if (suitableAsset) {
-                    this.downloadURL = suitableAsset.browser_download_url;
-                    this.assetAvailable = true;
-                }
-            });
-    }
-});
+  })
+);
