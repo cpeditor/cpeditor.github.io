f6a86f2da057ab24dde666cf92ad6f24bbd70d2c
Yufan You <ouuansteve@gmail.com>
2021-02-07 00:25:07 +0800

    build: reverse versions

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 git config core.quotePath false
 
-version_branches="$(git for-each-ref --format='%(refname:lstrip=3)' refs/remotes/*/v*.* | sort -u)"
+version_branches="$(git for-each-ref --format='%(refname:lstrip=3)' refs/remotes/*/v*.* | sort -Vur)"
 
 function build() {
     if [[ "$1" != "" ]]; then
@@ -14,12 +14,12 @@ function build() {
 
     echo "[[params.versions]]
     version = \"alpha\"
-    url = \"https://cpeditor.org/\"" >> config.toml
+    url = \"https://cpeditor.org/\"" >>config.toml
 
     for version in $version_branches; do
         echo "[[params.versions]]
         version = \"$version\"
-        url = \"https://cpeditor.org/$version\"" >> config.toml
+        url = \"https://cpeditor.org/$version\"" >>config.toml
     done
 
     hugo --minify
@@ -33,7 +33,8 @@ function build() {
 
 git checkout hugo
 build
-rm -rf dist; mv public dist
+rm -rf dist || true
+mv public dist
 
 for branch in $version_branches; do
     git checkout "$branch"
